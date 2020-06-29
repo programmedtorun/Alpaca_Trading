e3e27ead6b2e7ebe72ce43914dc59929012c6539
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-29 16:27:52 -0400

    getting account info returned from api

--- req_file.py
+++ req_file.py
@@ -1,4 +1,6 @@
 import json
+import requests
+
 
 
 secrets_filename = 'KEY_FILE.json'
@@ -6,4 +8,15 @@ api_keys = {}
 with open(secrets_filename, 'r') as f:
     api_keys = json.loads(f.read())
 
-print(api_keys['API_KEY_ID'])
+# print(api_keys['API_KEY_ID'])
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+
+r = requests.get(ACCOUNT_URL, headers=
+                {"APCA-API-KEY-ID": api_keys['API_KEY_ID'],
+                "APCA-API-SECRET-KEY": api_keys['SECRET_KEY']}
+                 )
+
+print(r.content)
+
