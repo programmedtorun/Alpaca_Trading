91bf8ae5a2d6b77927075b8a5d5033e469dfaa03
Patrick Skelley <songkick.skelley@gmail.com>
2020-11-27 14:30:39 -0500

    adds stock order function

--- req_file.py
+++ req_file.py
@@ -2,10 +2,15 @@ import json
 import requests
 import os
 import alpaca_trade_api as tradeapi
+import logging
+
+
+
+logging.basicConfig(filename='req_file.log', format='%(asctime)s %(message)s', level=logging.INFO)
 #sys.path.insert(0, "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages")
 # Note: you need to install pytz for alpaca_trade_api to work on your system `pip3 install pytz` if it already exists then you may
 # have to manually update it by deleting the pytz.<blah>.dist-info and installing again.
-
+logging.warning('%s before you %s', 'Look', 'leap!')
 #  ------------- Setting Key, Secret, Base URL -------------
 secrets_filename = 'KEY_FILE.json'
 api_keys = {}
@@ -38,9 +43,30 @@ def acct_status():
 
     active_assets = api.list_assets(status='active', asset_class='us_equity')
     print("ACTIVE ASSET LISTING")
-    for a in active_assets:
-        print(a.symbol)
+    # for a in active_assets:
+    #     print(a.symbol)
     print("Account status is --> {}".format(account.status))
+
+#  ------------- order stock -------------
+def buy(symbol, side, type, qty, time_in_force):
+    api = tradeapi.REST()
+    # all_pos = api.list_positions()
+    # print(all_pos)
+    # snow_pos = api.get_position(symbol)
+    # print("position in SNOW is: {}".format(snow_pos))
+    order = api.submit_order(
+        symbol=symbol,
+        side=side,
+        type=type,
+        qty=qty,
+        time_in_force=time_in_force
+    )
+    print("order is: {}".format(order))
+    p_hist = api.get_portfolio_history()
+    # print("portfolio history is: {}".format(p_hist))
 
 
 acct_status()
+# buy IBM stock
+# buy('IBM', 'buy', 'market', 20, 'day')
+
