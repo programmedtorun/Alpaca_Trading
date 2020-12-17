259a6e97cf212868bd1ce0b2be6e044ce6c51b81
Patrick Skelley <songkick.skelley@gmail.com>
2020-12-17 18:17:30 -0500

    printing acct info - live trading account active

--- live_req_file.py
+++ live_req_file.py
@@ -3,33 +3,23 @@ import requests
 import os
 import alpaca_trade_api as tradeapi
 import logging
+import datetime
 
 
 
-logging.basicConfig(filename='req_file.log', format='%(asctime)s %(message)s', level=logging.INFO)
-#sys.path.insert(0, "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages")
-# Note: you need to install pytz for alpaca_trade_api to work on your system `pip3 install pytz` if it already exists then you may
-# have to manually update it by deleting the pytz.<blah>.dist-info and installing again.
-logging.warning('%s before you %s', 'Look', 'leap!')
+logging.basicConfig(filename='live_req_file.log', format='%(asctime)s %(message)s', level=logging.INFO)
+
+logging.warning('%s before you %s, your live request file has been initiated on %s', 'Look', 'leap', datetime.datetime.now())
 #  ------------- Setting Key, Secret, Base URL -------------
-secrets_filename = 'KEY_FILE.json'
+secrets_filename = 'LIVE_KEY_FILE.json'
 api_keys = {}
 with open(secrets_filename, 'r') as f:
     api_keys = json.loads(f.read())
 os.environ["APCA_API_KEY_ID"] = api_keys['API_KEY_ID']
 os.environ["APCA_API_SECRET_KEY"] = api_keys['SECRET_KEY']
-os.environ["APCA_API_BASE_URL"] = "https://paper-api.alpaca.markets"
+os.environ["APCA_API_BASE_URL"] = "https://api.alpaca.markets"
 
 
-#  ------------- Using requests python lib -------------
-BASE_URL = "https://paper-api.alpaca.markets"
-ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
-
-r = requests.get(ACCOUNT_URL, headers=
-                {"APCA-API-KEY-ID": api_keys['API_KEY_ID'],
-                "APCA-API-SECRET-KEY": api_keys['SECRET_KEY']}
-                 )
-print("requests.models.Response object - Account information --> {}".format(r.content))
 
 
 #  ------------- Using alpaca_trade_api -------------
@@ -39,7 +29,8 @@ def acct_status():
     if account.trading_blocked:
         print("account is blocked")
     else:
-        print("account is not blocked")
+        print("account is not blocked INFO::::")
+        print(account)
 
     active_assets = api.list_assets(status='active', asset_class='us_equity')
     print("ACTIVE ASSET LISTING")
